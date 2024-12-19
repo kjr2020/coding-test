d9e0e5d80576b3f6aa2dee03dfc6472a3b4a27e3
kjr2020 <kjr202020@gmail.com>
2024-12-20 01:17:45 +0900

    feat(Practice_4): 프로그래머스 - 없는 숫자 더하기

---
+++ Java/src/Practice_4.java
@@ -0,0 +1,16 @@
+public class Practice_4 {
+    /*
+    * 프로그래머스
+    * 없는 숫자 더하기
+    */
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4,6,7,8,0};
+        int answer = 45;
+
+        for(int number : numbers){
+            answer -= number;
+        }
+
+        return answer;
+    }
+}
