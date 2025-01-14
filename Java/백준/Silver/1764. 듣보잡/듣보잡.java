417e6f9f57cb4b832db80bc427b75acf61cea4e5
HJLee <kjr202020@gmail.com>
2025-01-14 15:22:23 +0900

    [Silver IV] Title: 듣보잡, Time: 272 ms, Memory: 26480 KB -BaekjoonHub

--- Java/백준/Silver/1764. 듣보잡/듣보잡.java
+++ Java/백준/Silver/1764. 듣보잡/듣보잡.java
@@ -1,5 +1,3 @@
-// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
 import java.io.*;
 import java.util.*;
 
@@ -9,34 +7,22 @@ class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-        ArrayList<String> notHeard = new ArrayList<String>();
-        ArrayList<String> notSee = new ArrayList<String>();
+        Map<String, Boolean> notHeard = new HashMap<>();
         int answerNum = 0;
         ArrayList<String> answer = new ArrayList<String>();
         
         for(int i = 0 ; i < N ; i++){
-            notHeard.add(br.readLine());
+            notHeard.put(br.readLine(), true);
         }
-        Collections.sort(notHeard);
-        for(int i = 0 ; i < M ; i ++ ){
-            notSee.add(br.readLine());
-        }
-        Collections.sort(notSee);
         
-        for(int i = 0 ; i < N ; i++){
-            for(int j = 0 ; j < notSee.size() ; j++){
-                if(notHeard.get(i).equals(notSee.get(j))){
-                    answer.add(notSee.get(j));
-                    notSee = new ArrayList<>(notSee.subList(j+1, notSee.size()));
-                    answerNum++;
-                    break;
-                }
-                if(notHeard.get(i).compareTo(notSee.get(j)) < 0) {
-                    break;
-                }
+        for(int i = 0 ; i < M ; i++){
+            String target = br.readLine();
+            if(notHeard.containsKey(target)){
+                answerNum++;
+                answer.add(target);
             }
         }
-        
+        Collections.sort(answer);
         System.out.println(answerNum);
         for(String an : answer){
             System.out.println(an);
