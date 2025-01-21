0563d55626a52a11b6a1bd9af8ec0b106cc28168
HJLee <kjr202020@gmail.com>
2025-01-22 01:29:04 +0900

    [Bronze I] Title: 최소공배수, Time: 120 ms, Memory: 14656 KB -BaekjoonHub

--- Java/백준/Bronze/1934. 최소공배수/최소공배수.java
+++ Java/백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -6,26 +6,36 @@ class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
-        int result = 1;
+        //int result = 1;
         
         for(int i = 0 ; i < T ; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             int first = Integer.parseInt(st.nextToken());
             int second = Integer.parseInt(st.nextToken());
-            int small = first < second ? first : second;
-            for(int j = 2 ; j <= small ; j++){
-                if(first%j == 0 && second%j == 0){
-                    result *= j;
-                    small /= j;
-                    first /= j;
-                    second /= j;
-                    j--;
-                }
-            }
-            result = result * first * second;
-            sb.append(result + "\n");
-            result = 1;
+            // int small = first < second ? first : second;
+            // for(int j = 1 ; j <= small ; j++){
+            //     if(first%j == 0 && second%j == 0){
+            //         result *= j;
+            //         small /= j;
+            //         first /= j;
+            //         second /= j;
+            //     }
+            // }
+            // result = result * first * second;
+            // sb.append(Integer.toString(result) + "\n");
+            // result = 1;
+            int gcd = getGCD(first, second);
+            int result = (first * second)/gcd;
+            sb.append(result).append("\n");
         }
         System.out.println(sb);
+    }
+    private static int getGCD(int a, int b){
+        while(b != 0){
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
     }
 }
