48617e212181e5f3c784f37023e7332e58441053
HJLee <kjr202020@gmail.com>
2025-05-25 02:13:50 +0900

    [Silver II] Title: 병사 배치하기, Time: 136 ms, Memory: 14736 KB -BaekjoonHub

---
+++ Java/백준/Silver/18353. 병사 배치하기/병사 배치하기.java
@@ -0,0 +1,33 @@
+/**
+LIS(Longest Increasing Subsequence)를 이용해 LDS(Longest Decreasing Subsequence)를 구함
+**/
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] soldArr = new int[n+1];
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, 1);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1 ; i <= n ; i++){
+            soldArr[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (soldArr[i] < soldArr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int answer = dp[1];
+        for(int i = 2 ; i <= n ; i++){
+            answer = Math.max(answer, dp[i]);
+        }
+        
+        System.out.println(n - answer);
+    }
+}
